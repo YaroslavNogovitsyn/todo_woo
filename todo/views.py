20f3cac25cbc3a1ce8ce3c823c744cb6e82b3fc1
Yaroslav <yaroslav.nogovitsyn@yandex.ru>
2023-04-30 20:52:00 +0300

    isort & flake8

--- todo/views.py
+++ todo/views.py
@@ -1,10 +1,10 @@
-from django.db import IntegrityError
-from django.shortcuts import render, redirect, get_object_or_404
-from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
-from django.contrib.auth.models import User
-from django.contrib.auth import login, logout, authenticate
-from django.utils import timezone
+from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.contrib.auth.models import User
+from django.db import IntegrityError
+from django.shortcuts import get_object_or_404, redirect, render
+from django.utils import timezone
 
 from .forms import ToDoForm
 from .models import ToDo
